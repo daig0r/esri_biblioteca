75d3e61f046ee4f417da7dab1dca411fd91c1324
Diego Alejandro Rodriguez Ayala <daigor.dev@gmail.com>
2019-12-13 13:25:50 -0500

    Init login page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,18 @@
-<template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title>Biblioteca</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn text>Inicio</v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
-    <nuxt />
-  </v-app>
+<template lang="pug">
+  v-app
+    v-app-bar(app)
+      nuxt-link(to="/")
+        v-img.mx-4(
+          :src="require('~/assets/logo_esri_colombia_negro.png')"
+          max-height="40"
+          max-width="180"
+          contain
+        )
+      v-toolbar-title.headline.font-weight-bold Biblioteca
+      v-spacer
+      v-toolbar-items
+        v-container
+          v-btn(color="primary") Login
+    v-content
+        nuxt
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+    v-container(fluid)
+        v-row(
+            align="center"
+            justify="center"
+        )
+            v-col(
+                cols="12"
+                sm="8"
+                md="6"
+                lg="4"
+            )
+                v-card(
+                    elevation="12"
+                )
+                    v-toolbar(
+                        color="primary"
+                        dark
+                        flat
+                    )
+                        v-toolbar-title Login
+                    v-card-text
+                        v-form
+                            v-text-field(
+                                label="Email"
+                                prepend-icon="mdi-account"
+                            )
+                            v-text-field(
+                                :type="showPass ? 'text' : 'password'"
+                                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                                prepend-icon="mdi-lock"
+                                label="Constraseña"
+                                @click:append="showPass = !showPass"
+                            )
+                    v-card-actions
+                        v-spacer      
+                        v-btn(
+                            color="primary"
+                        ) Login  
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPass: false
+    }
+  }
+}
+</script>
